16c5423ef808d546d58e5efe80c0b23a3ae918cb
wegolev <a.wegolev@gmail.com>
2025-05-14 07:05:50 +0300

    3. API (DRF) 3.2. ViewSets DONE on branch step0

--- backend/orders/views.py
+++ backend/orders/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets, permissions
+from .models import Order, OrderItem
+from .serializers import OrderSerializer, OrderItemSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+class OrderViewSet(viewsets.ModelViewSet):
+    serializer_class = OrderSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
+    def get_queryset(self):
+        return Order.objects.filter(customer=self.request.user)
+    
+    def perform_create(self, serializer):
+        serializer.save(customer=self.request.user)
+    
+    @action(detail=True, methods=['post'])
+    def add_item(self, request, pk=None):
+        order = self.get_object()
+        # Логика добавления товара в заказ
+        return Response({'status': 'item added'})
--- backend/products/views.py
+++ backend/products/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+from .models import Category, Product
+from .serializers import CategorySerializer, ProductSerializer
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.filter(available=True)
+    serializer_class = ProductSerializer
+    
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        category = self.request.query_params.get('category')
+        if category:
+            queryset = queryset.filter(category__id=category)
+        return queryset
+    
+
+    from rest_framework import viewsets
+from .models import Category, Product
+from .serializers import CategorySerializer, ProductSerializer
