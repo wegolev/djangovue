0c70ce71fb580cb0c6c73a854f023100e38c2a70
wegolev <a.wegolev@gmail.com>
2025-05-15 10:19:44 +0300

    del old frontend

--- frontend/src/components/AppFooter.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <v-footer color="primary" padless>
-      <v-col class="text-center white--text" cols="12">
-        © {{ new Date().getFullYear() }} Магазин стройматериалов
-      </v-col>
-    </v-footer>
-  </template>
